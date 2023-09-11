c28844800e724e695309b27204088f76cf7fad0b
Dane Wilson <dnkw@hotmail.com>
2023-09-11 15:23:11 -0500

    Fix error page
    Signed-off-by: Dane Wilson <dnkw@hotmail.com>

--- src/shared/ErrorPage.jsx
+++ src/shared/ErrorPage.jsx
@@ -1,4 +1,21 @@
+import './ErrorPage.scss';
+
 const ErrorPage = () => {
-  return <>Error</>;
+  return (
+    <>
+      <div className="error-page">
+        <div className="content">
+          <div className="lead">
+            <h1 className="error-title">404</h1>
+            <p className="error-subtitle">Page Not Found</p>
+          </div>
+          <p>
+            Hit the back button or click <a href="/">here</a> to go the home
+            page.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 export default ErrorPage;
